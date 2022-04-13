55fa7f886d831e9fd72101211c09b50a7a97d4d7
zysam <explnwind@gmail.com>
2022-04-13 12:59:04 +0800

    feat: init my template

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react'
+import './index.css'
+
+export const App: FC = () => {
+  return (
+    <div className="absolute h-full w-full flex items-center justify-center bg-gray-200">
+      <span className="text-3xl">Hello, world</span>
+    </div>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,7 @@
-import React, { FC } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import { App } from './App'
 
-const App = document.getElementById('root')
-const Root: FC = () => {
-  return (
-    <div className="absolute h-full w-full flex items-center justify-center bg-gray-200">
-      <span className="text-3xl">Hello, world</span>
-    </div>
-  )
-}
-ReactDOM.render(<Root />, App)
+const AppId = document.getElementById('root')
+
+ReactDOM.render(<App />, AppId)
