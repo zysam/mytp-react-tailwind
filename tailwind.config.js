3a457901124587b964bf135bd962bf15baef521f
zysam <explnwind@gmail.com>
2022-04-13 13:08:25 +0800

    feat: add daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,49 +1,26 @@
 module.exports = {
-  purge: ['./src/**/*.html', './src/**/*.vue', './src/**/*.tsx'],
-  darkMode: 'class',
   mode: 'jit',
-  important: true, // important in prod is must be
-
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    safeList: [],
+    content: ['./index.html', './src/**/*.tsx', './src/**/*.ts'],
+  },
   theme: {
-    extend: {
-      screens: {
-        'light-mode': { raw: '(prefers-color-scheme: light)' },
-        'dark-mode': { raw: '(prefers-color-scheme: dark)' },
-        phone: { raw: '(max-width: 768px)' },
-        desktop: { raw: '(min-width: 1024px)' },
-        tablet: { raw: '(max-width: 1023px)' },
-      },
-      zIndex: {
-        '-10': -10,
-        '-1': -1,
-        0: 0,
-        1: 1,
-        10: 10,
-        20: 20,
-        30: 30,
-        40: 40,
-        50: 50,
-        60: 60,
-        70: 70,
-        80: 80,
-        90: 90,
-        100: 100,
-        auto: 'auto',
-      },
-      colors: {
-        primary: {
-          default: '#18A058FF',
-          deep: '#0C7A43FF',
-          shallow: '#36AD6AFF',
-        },
-        gray$: {
-          default: '#ddd',
-        },
-      },
+    minWidth: {
+      40: '10rem',
+      60: '15rem',
+      80: '20rem',
+      100: '25rem',
+    },
+    maxWidth: {
+      120: '30rem',
+      160: '40rem',
+      200: '50rem',
     },
   },
-  variants: {
-    extend: {},
+  variants: {},
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ['emerald'],
   },
-  plugins: [],
 }
